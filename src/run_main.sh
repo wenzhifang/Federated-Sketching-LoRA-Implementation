fa923c7a17c10bf508b02fb4682a96813cf0f27f
wenzhi fang <39112845+wenzhifang@users.noreply.github.com>
2025-03-31 22:52:39 -0400

    Update run_main.sh

--- src/run_main.sh
+++ src/run_main.sh
@@ -3,7 +3,7 @@
 export TOKENIZERS_PARALLELISM=true
 rank_type="normal"
 
-accelerate launch --num_processes 4 main.py --client-batch 4 --lora_r 64 --clients 10 --num_epochs 15 --num_comm_rounds 30 --local_iter_per_round 20 --rank_type $rank_type
+accelerate launch --num_processes 4 main.py --client-batch 4 --lora_r 64 --clients 50 --num_epochs 15 --num_comm_rounds 30 --local_iter_per_round 20 --rank_type $rank_type
 
 export CUDA_VISIBLE_DEVICES=0
 
