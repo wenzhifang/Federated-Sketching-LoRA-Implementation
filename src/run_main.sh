5d1fe21daec3f4e29d9ad4fbd8562c27f27c4045
wenzhi fang <39112845+wenzhifang@users.noreply.github.com>
2025-05-11 01:05:32 -0400

    Update run_main.sh

--- src/run_main.sh
+++ src/run_main.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
 export TOKENIZERS_PARALLELISM=true
-rank_type="normal"
 
-accelerate launch --num_processes 4 main.py --client-batch 4 --lora_r 64 --clients 50 --num_epochs 15 --num_comm_rounds 30 --local_iter_per_round 20 --rank_type $rank_type
+rank_type="heavy_tail_low"
+num_comm_rounds=600
+local_iter_per_round=20
+eval_freq=20
 
-export CUDA_VISIBLE_DEVICES=0
+accelerate launch --num_processes 4 main.py \
+    --client-batch 4 \
+    --lora_r 64 \
+    --lora_r_min 1 \
+    --lora_r_max 32 \
+    --clients 50 \
+    --num_comm_rounds $num_comm_rounds \
+    --eval_freq $eval_freq \
+    --local_iter_per_round $local_iter_per_round \
+    --rank_type $rank_type
 
-python evaluation_vllm.py
+python evaluation_vllm.py --eval_freq $eval_freq --num_comm_rounds $num_comm_rounds --local_iter_per_round $local_iter_per_round --rank_type $rank_type --algorithm "slora64"
